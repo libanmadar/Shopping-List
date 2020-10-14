5c0092131a8b97e5a97cb5cf6dc22ae4ed808bb5
unknown <liiban333@gmail.com>
2020-10-14 13:48:52 -0500

    connected to mongo cluster

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Liban:Liban1234@cluster0.tidj6.mongodb.net/shopping?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI
+
+mongoose.connect(db,{ useNewUrlParser: true,useUnifiedTopology: true })
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`))
+
