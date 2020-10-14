a69b8b8ca475d3d3a64f8c07776c7eef79a9b5c1
unknown <liiban333@gmail.com>
2020-10-14 14:36:59 -0500

    fixed syntax

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(db,{ useNewUrlParser: true,useUnifiedTopology: true })
 
 app.use('/api/items', items)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`))
 
