87bda40df912dd9cd83b07a11c4d00b1e028be11
unknown <liiban333@gmail.com>
2020-10-14 14:19:44 -0500

    created item post route

--- routes/api/items.js
+++ routes/api/items.js
@@ -3,10 +3,20 @@ const router = express.Router();
 
 const Item = require ('../../models/item');
 
+// get all items
 router.get('/', (req, res) => {
     Item.find()
     .sort({ date: -1 })
     .then(items => res.json(items));
+});
+
+//create item
+router.post('/', (req, res) => {
+    const newItem = new Item({
+       name: req.body.name 
+    });
+
+    newItem.save().then(item => res.json(item));
 });
 
 module.exports = router;
