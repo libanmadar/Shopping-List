fea1e6409318d161abb47f9010757b3407835026
unknown <liiban333@gmail.com>
2020-10-15 11:25:53 -0500

    created store component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Navbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList'
 
+import { Provider, provider } from 'react-redux';
+import store from './store'
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    <Provider>
     <div className="App">
      <Navbar />
      <ShoppingList />
     </div>
+    </Provider>
   );
 }
 
